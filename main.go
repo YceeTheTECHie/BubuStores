89ee3395fcd016bacb40e9783a829c513ebf2edf
Boluwatife Ayodele <waplurd1@gmail.com>
2021-04-27 13:16:59 +0100

    defined API routes

--- main.go
+++ main.go
@@ -12,6 +12,13 @@ func main() {
 
 	r := mux.NewRouter()
 
-	
+	// defining routes
+
+	r.HandleFunc("/api/products", getProducts).Methods("GET")
+	r.HandleFunc("/api/products", createProduct).Methods("POST")
+	r.HandleFunc("/api/products/{id}", getProduct).Methods("GET")
+	r.HandleFunc("/api/products/{id}", updateProduct).Methods("PUT")
+	r.HandleFunc("/api/products", deleteProduct).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":5000", r))
 }
 
