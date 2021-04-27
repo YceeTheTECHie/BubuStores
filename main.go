745a3681d70265b2e91c3889142b1824803f44e8
Boluwatife Ayodele <waplurd1@gmail.com>
2021-04-27 18:29:37 +0100

    Added a pathprefix to the routes for easy versioning

--- main.go
+++ main.go
@@ -13,12 +13,12 @@ func main() {
 	r := mux.NewRouter()
 
 	// defining routes
-
-	r.HandleFunc("/api/products", getProducts).Methods("GET")
-	r.HandleFunc("/api/products", createProduct).Methods("POST")
-	r.HandleFunc("/api/products/{id}", getProduct).Methods("GET")
-	r.HandleFunc("/api/products/{id}", updateProduct).Methods("PUT")
-	r.HandleFunc("/api/products", deleteProduct).Methods("DELETE")
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("/products", getProducts).Methods("GET")
+	api.HandleFunc("/products/create", createProduct).Methods("POST")
+	api.HandleFunc("/products/{id}", getProduct).Methods("GET")
+	api.HandleFunc("/products/{id}", updateProduct).Methods("PUT")
+	api.HandleFunc("/products", deleteProduct).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":5000", r))
 }
 
